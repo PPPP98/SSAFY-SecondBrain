2cde47637625910011659d22a2711303dfa9f5e7
LEESEOKJAE <lsj6924@naver.com>
2025-11-04 15:59:58 +0900

    [BE] feat: reminder를 위한 repository 메서드 추가 및 예외 응답 코드 추가

--- backend/secondbrain/src/main/java/uknowklp/secondbrain/api/note/repository/NoteRepository.java
+++ backend/secondbrain/src/main/java/uknowklp/secondbrain/api/note/repository/NoteRepository.java
@@ -1,8 +1,16 @@
 package uknowklp.secondbrain.api.note.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import uknowklp.secondbrain.api.note.domain.Note;
 
 public interface NoteRepository extends JpaRepository<Note, Long> {
+
+	// N+1 방지를 위한 fetch join
+	@Query("select n from Note n join fetch n.user where n.id = :noteId")
+	Optional<Note> findByIdWithUser(@Param("noteId") Long noteId);
 }
