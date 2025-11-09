698e05be8cc7fc315ed9cd618b679d87f3374772
PPPP98 <dodwn48@gmail.com>
2025-11-09 22:08:38 +0900

    [AI] feat: 의존성 업데이트 / State, model 정의 - url 추출 라이브러리 설치 - summarize model, graph state 정의

---
+++ knowledge-graph-service/app/agents/note_summarize_agent/models.py
@@ -0,0 +1,22 @@
+from langchain.chat_models import init_chat_model
+from app.core.config import get_settings
+import os
+
+
+class Model:
+    """
+    ## LLM 모델 정의
+    - init : 환경변수 정의
+    - summarize_model : 요약 에이전트 선언
+    """
+    def __init__(self):
+        self.settings = get_settings()
+        os.environ["OPENAI_API_KEY"] = self.settings.openai_api_key
+        os.environ["OPENAI_API_BASE"] = self.settings.openai_base_url
+
+    def summarize_model(self):
+        model = init_chat_model(
+            model=self.settings.summarize_model,
+            temperature=self.settings.summarize_temperature,
+        )
+        return model
---
+++ knowledge-graph-service/app/agents/note_summarize_agent/nodes.py
@@ -0,0 +1,4 @@
+from langchain.messages import SystemMessage
+from app.agents.note_summarize_agent.state import State
+
+
