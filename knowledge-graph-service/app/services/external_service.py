4333db023ab5296b0a07eb4f73f88992643c65ca
PPPP98 <dodwn48@gmail.com>
2025-11-15 14:56:31 +0900

    [AI] feat: 요약 에이전트 비동기 최적화

--- knowledge-graph-service/app/services/external_service.py
+++ knowledge-graph-service/app/services/external_service.py
@@ -2,6 +2,7 @@ from fastapi import HTTPException
 from app.core.config import get_settings
 import logging
 import requests
+import httpx
 
 settings = get_settings()
 
@@ -12,7 +13,11 @@ class ExternalService:
     def __init__(self):
         self.api_url = settings.secondbrain_api_url
 
-    def post_call_external_service(self, auth_token: str, payload: dict) -> dict:
+    def post_call_external_service(
+        self,
+        auth_token: str,
+        payload: dict,
+    ) -> dict:
         URL: str = f"{self.api_url}notes"
         headers = {
             "Authorization": f"Bearer {auth_token}",
@@ -22,6 +27,28 @@ class ExternalService:
             response = requests.post(URL, json=payload, headers=headers)
             response.raise_for_status()
             return response.json()
+
+        except Exception as e:
+            logger.error(f"External service call failed: {e}")
+            raise HTTPException(
+                status_code=500, detail=f"External service call failed: {e}"
+            )
+
+    async def async_post_call_external_service(
+        self,
+        auth_token: str,
+        payload: dict,
+    ) -> dict:
+        URL: str = f"{self.api_url}notes"
+        headers = {
+            "Authorization": f"Bearer {auth_token}",
+            "Content-Type": "application/json",
+        }
+        try:
+            async with httpx.AsyncClient() as client:
+                response = await client.post(URL, json=payload, headers=headers)
+                response.raise_for_status()
+                return response.json()
 
         except Exception as e:
             logger.error(f"External service call failed: {e}")
